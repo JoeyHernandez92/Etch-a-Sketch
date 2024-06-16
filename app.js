05421b65160084f066a74fa2be2b033178e04581
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-06-16 17:37:52 -0500

    starting on button eventListener

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const container = document.getElementById("grid-container");
 
+const button = document.querySelector('button');
+
+//needs implementation to alert and change grid
+button.addEventListener('change grid', () => {
+
+});
+
 function makeRows(rows, cols){
     for(let i = 0; i < rows * cols; i++){
         const cell = document.createElement('div');
