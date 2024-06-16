e452dc67e535f840399d5a2f764d1de7be73a802
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-06-16 17:02:39 -0500

    Grid has appeared need to format

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const container = document.getElementById("container");
+const container = document.getElementById("grid-container");
 
 function makeRows(rows, cols){
-
+    for(let i = 0; i < rows * cols; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        container.appendChild(cell);
+    }
 }
+
+makeRows(8,8);
