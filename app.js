ec93e71b58eb7fc95f24f3bf329748c03f4a1527
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-06-25 16:56:59 -0500

    working like its supposed to.

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ button.addEventListener('click', () => {
     
     if(rows && columns && !isNaN(rows) && !isNaN(columns)){
         container.innerHTML = '';
+        makeRows(parseInt(rows), parseInt(columns));
+    }
+    else{
+        alert("Please enter a valid number value");
     }
 });
 
-function makeRows(rows, cols){
-    for(let i = 0; i < rows * cols; i++){
+function makeRows(rows, columns){
+    container.style.display = 'grid';
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
+
+    for(let i = 0; i < rows * columns; i++){
         const cell = document.createElement('div');
         cell.classList.add('grid-cell');
         container.appendChild(cell);
