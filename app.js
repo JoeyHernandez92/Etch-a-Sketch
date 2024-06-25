b4aeb013da3f92c70924a840bccc8645504179b6
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-06-25 16:29:59 -0500

    prompts and clears out first grid dimensions

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const container = document.getElementById("grid-container");
 const button = document.querySelector('button');
 
 //needs implementation to alert and change grid
-button.addEventListener('change grid', () => {
-
+button.addEventListener('click', () => {
+    const rows = prompt("Enter the number of Rows you would like.");
+    const columns = prompt("Enter the number of columns you would like.");
+    
+    if(rows && columns && !isNaN(rows) && !isNaN(columns)){
+        container.innerHTML = '';
+    }
 });
 
 function makeRows(rows, cols){
